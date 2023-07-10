b01c4e3d5f4d3ceafb2fbcbfc5599eefdd0f6342
Edson <137941940+Edson-D-Lucas@users.noreply.github.com>
2023-07-10 18:20:39 -0300

    Add files via upload

---
+++ prova-pratica_node.hs/Prova_pratica/index.js
@@ -0,0 +1,27 @@
+// QUESTÃO 01
+
+// const http = require('http')
+
+// const port = 3000
+
+// const serve = http.createServer((req, res) =>{
+//     res.end();
+// })
+
+// serve.listen({
+//     host: 'localhost',
+//     port: 3000
+// })
+
+// QUESTÃO 02
+
+// const json = JSON.stringify(objeto)
+// console.log(json)
+
+
+// const url = require('url')
+
+// const newURL = new URL ('https:al.senai.br/cursos/tecnico-em-informatica-para-internetplanodecurso.pdf')
+
+// newURL.pathname(newURL)
+
